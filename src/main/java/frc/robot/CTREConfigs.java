39833b0245f329dc0ae165793759b352a13b73b4
FullMetalJackets <apaulson@perham.k12.mn.us>
2024-01-22 17:41:00 -0600

    swerveCanCoderConfig changes

--- src/main/java/frc/robot/CTREConfigs.java
+++ src/main/java/frc/robot/CTREConfigs.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 
@@ -18,11 +19,13 @@ public final class CTREConfigs {
     public static TalonFXConfiguration swerveAngleFXConfig;
     public static TalonFXConfiguration swerveDriveFXConfig;
     public static CANcoderConfiguration swerveCanCoderConfig;
+    public static MagnetSensorConfigs magnetSensorConfigs;
 
     public CTREConfigs(){
         swerveAngleFXConfig = new TalonFXConfiguration();
         swerveDriveFXConfig = new TalonFXConfiguration();
         swerveCanCoderConfig = new CANcoderConfiguration();
+        magnetSensorConfigs = new MagnetSensorConfigs();
 
 // var fx_cfg = new TalonFXConfiguration();
 // // fetch *all* configs currently applied to the device
@@ -72,10 +75,12 @@ public final class CTREConfigs {
         // swerveCanCoderConfig.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
         // swerveCanCoderConfig.sensorTimeBase = SensorTimeBase.PerSecond;
         
-        swerveCanCoderConfig.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
-        swerveCanCoderConfig.MagnetSensor.SensorDirection = SwerveConstants.Swerve.canCoderInvert;
+        // swerveCanCoderConfig.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
+        // swerveCanCoderConfig.MagnetSensor.SensorDirection = SwerveConstants.Swerve.canCoderInvert;
         // In Phoenix 6, Talon FX and CANcoder sensors are always initialized to their absolute position.
         // In Phoenix 6, CANcoder does not support setting a custom sensor coefficient, unit string, and sensor time base.
-
+        magnetSensorConfigs.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
+        magnetSensorConfigs.SensorDirection = SwerveConstants.Swerve.canCoderInvert;
+        swerveCanCoderConfig.withMagnetSensor(magnetSensorConfigs);
     }
 }
